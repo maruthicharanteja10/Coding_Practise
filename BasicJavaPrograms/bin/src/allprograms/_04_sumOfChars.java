1a27d051c2cef7d486f231910501db9a8f003942
maruthicharanteja <124443822+maruthicharanteja10@users.noreply.github.com>
2025-03-08 12:32:12 +0530

    Delete BasicJavaPrograms/bin/src directory

--- BasicJavaPrograms/bin/src/allprograms/_04_sumOfChars.java
+++
@@ -1,9 +0,0 @@
-package allprograms;
-
-public class _04_sumOfChars {
-	public static void main(String[] args) {
-     char c1='1';
-     char c2='8';
-     System.out.println((c1-48)+(c2-48));
-	}
-}
